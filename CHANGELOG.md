4b5a658bcb34e8b757bf566e636173a955cfa1b7
Flavio Pellizzer <flappio.pelliccia@gmail.com>
2024-09-16 11:30:47 +0200

    refactoring Qapla' v1.2 w/ tests to and respect SOLID principle. Its incomplete but just to have a schema

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable changes to `laravel-qapla` will be documented in this file.
 
 Updates should follow the [Keep a CHANGELOG](http://keepachangelog.com/) principles.
 
+## [2.0.0] - 2024-09-16
+- Updated to minimum Laravel version 6.0
+- Updated to minimum PHP version 7.2
+- Updated API Qapla' to version 1.2 (https://api.qapla.dev/1.2/)
+- First release CI/CD pipeline, only just started and not yet fully operational
+- Rewritten the entire package to be compatible with PHP CS Fixer and PHPStan (PSR-12 and PSR-4)
+- Update Composer dependencies
+
 ## [1.0.0] - 2018-04-16
 
 ### Added
---
+++ tests/W3design/Qapla/Api/QaplaApiV12Test.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Laravel Qapla
+ * Copyright since 2024 Flavio Pellizzer and Contributors
+ * <Silvano Fabbro> Property
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/MIT
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to flappio.pelliccia@gmail.com so we can send you a copy immediately.
+ *
+ * @author    Flavio Pellizzer <flappio.pelliccia@gmail.com>
+ * @copyright Since 2024 Flavio Pellizzer
+ * @license   https://opensource.org/licenses/MIT
+ */
+declare(strict_types=1);
+
+namespace W3design\Qapla\Api;
+
+use PHPUnit\Framework\TestCase;
+
+class QaplaApiV12Test extends TestCase
+{
+    public function testEmpty(): array
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPushOrder(array $stack): array
+    {
+        $stack[] = 'order';
+        $this->assertSame('order', $stack[count($stack) - 1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    public function testGetOrder()
+    {
+        // Optional: Test anything here, if you want.
+        $this->assertTrue(true, 'This should already work.');
+
+        // Stop here and mark this test as incomplete.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    public function testGetOrdersIsArray()
+    {
+        $api = new QaplaApiV12('your-api-key');
+        $orders = $api->getOrders();
+
+        $this->assertIsArray($orders);
+    }
+
+    public function testDeleteOrder()
+    {
+        // Optional: Test anything here, if you want.
+        $this->assertTrue(true, 'This should already work.');
+
+        // Stop here and mark this test as incomplete.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    public function testUndeleteOrder()
+    {
+        // Optional: Test anything here, if you want.
+        $this->assertTrue(true, 'This should already work.');
+
+        // Stop here and mark this test as incomplete.
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
